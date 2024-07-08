6b5905a258bbcf149152e50e31ed8ef9986fb6de
m-burnett <161596436+m-burnett@users.noreply.github.com>
2024-07-08 09:44:34 +0100

    tester proto on megan branch

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,17 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+//do you go by another name
+router.post('/megan/anotherjobquestionAnswer', function(request, response) {
+
+    var anotherjob = request.session.data["anotherjob"];
+    
+    if (anotherjob == "yes" )
+    {
+        response.redirect("/anotherjob")
+    }
+    else
+    {
+        response.redirect("/checkanswers")
+    }
+}) 
